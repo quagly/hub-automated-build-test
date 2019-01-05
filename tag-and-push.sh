67010de962c4eee9668cc0b64d7cead206e3d69e
Michael West <quagly@gmail.com>
2019-01-05 05:46:06 -0800

    use prefix v for version

--- tag-and-push.sh
+++ tag-and-push.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # tag repo with version from VERSION.txt file
+# and push tag
+# note that git does not push tags on a normal push
 set -euxo pipefail
 
 source VERSION.txt
 
 git tag $VERSION
+
+git push origin $VERSION
